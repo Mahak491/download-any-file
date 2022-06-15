f7f640bb7265a1bb37af3cb7e4adf70986052e9e
Mahak491 <shreemahak01@gmail.com>
2022-06-16 02:16:35 +0530

    file downloader

---
+++ script.js
@@ -0,0 +1,27 @@
+const fileInput = document.querySelector("input"),
+downloadbtn = document.querySelector("button");
+downloadbtn.addEventListener("click",e => {
+    e.preventDefault();//prevent form from submitting
+    downloadbtn.innerText = "Downloading file..."
+    fetchFile(fileInput.value);
+});
+
+function fetchFile(url){
+    fetch(url).then(res => res.blob()).then(file => {
+        //creates the url of passed object => createObject
+        let tempUrl = URL.createObjectURL(file);
+        let aTage = document.createElement("a");
+        aTage.href = tempUrl; //passing tempurl as href value of <a> tag 
+        //passing file lastname and extension as download value of <a> tage
+        aTage.download = url.replace(/^.*[\\\/]/,'');
+        document.body.appendChild(aTage); //adding <a> tag inside body
+        aTage.click();
+        aTage.remove();
+        URL.revokeObjectURL(tempUrl);//removing tempUrl from document
+        downloadbtn.innerText = "Download File";
+    }).catch(() => {
+        downloadbtn.innerText = "Download File";
+        alert("Failed to download file")
+    });
+}
+
